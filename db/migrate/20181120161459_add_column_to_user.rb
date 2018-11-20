8675b54d951eb8b92eae9d7d563ccf7d0725668d
Nader Kaddour <naderkaddour@gmail.com>
2018-11-20 14:46:04 -0500

    modified user table

--- db/migrate/20181120161459_add_column_to_user.rb
+++ db/migrate/20181120161459_add_column_to_user.rb
@@ -1,6 +1,7 @@
 class AddColumnToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string
+    add_column :users, :email, :string
     add_column :users, :photo, :string
     add_column :users, :is_mechanic, :boolean
     add_column :users, :rating, :integer
---
+++ db/migrate/20181120194327_rename_name_to_first_name.rb
@@ -0,0 +1,7 @@
+class RenameNameToFirstName < ActiveRecord::Migration[5.2]
+
+  def change
+    rename_column :users, :name, :first_name
+    add_column :users, :last_name, :string
+  end
+end
