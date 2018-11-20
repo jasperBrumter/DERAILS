adf1cc01c83d9561fcfc85d33273c3918bad7abe
Jasper Brumter <jasper.brumter@mail.mcgill.ca>
2018-11-20 16:58:18 -0500

    changed migration file of users

--- db/migrate/20181120161459_add_column_to_user.rb
+++ db/migrate/20181120161459_add_column_to_user.rb
@@ -1,9 +1,8 @@
 class AddColumnToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string
-    add_column :users, :email, :string
     add_column :users, :photo, :string
     add_column :users, :is_mechanic, :boolean
-    add_column :users, :rating, :integer
+    add_column :users, :rating, :float
   end
 end
