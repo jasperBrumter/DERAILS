f09b9e5ef1f3b0339d18ff92e42d0e7e824e50d4
Jasper Brumter <jasper.brumter@mail.mcgill.ca>
2018-11-26 12:53:20 -0500

    added facebook login

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem "pundit"
 gem 'rails', '5.2.1'
 gem 'redis'
 gem "geocoder"
+gem "omniauth-facebook"
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_170028) do
+ActiveRecord::Schema.define(version: 2018_11_26_171351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,8 +65,13 @@ ActiveRecord::Schema.define(version: 2018_11_22_170028) do
     t.string "first_name"
     t.string "photo"
     t.boolean "is_mechanic"
-    t.float "rating"
+    t.integer "rating"
     t.string "last_name"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
