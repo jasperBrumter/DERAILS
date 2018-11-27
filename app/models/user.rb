0799137fa8157a075836134dd0dbd46f950f18bb
Jasper Brumter <jasper.brumter@mail.mcgill.ca>
2018-11-27 15:58:22 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tickets, dependent: :destroy
   has_many :responses, dependent: :destroy
-  has_many :reviews, dependent: :destroy
+  has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
