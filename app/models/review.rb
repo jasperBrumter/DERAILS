a4447811e8f06ef8ca655b7441ef88d8e4756e28
Aneta Crescuicci <anetacres@gmail.com>
2018-11-29 17:58:17 +0100

    changed style and profile page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :user
-  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, presence: true, numericality: { only_integer: true}, inclusion: {in: [1, 2, 3, 4, 5]}
+  validates :content, presence: true
 end
